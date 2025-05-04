3dacaa57529bd31e0d27617c19575ca5dc017ede
Simone Serafini <63721643+SimoSera@users.noreply.github.com>
2025-05-04 17:41:03 +0200

    Update and rename VaseSensors.cpp to PotSensors.cpp
    Changed the class name to PotSensors

--- mqtt_client/PotSensors.cpp
+++ mqtt_client/PotSensors.cpp
@@ -10,13 +10,13 @@
 
 
 
-#include "VaseSensors.h"
+#include "PotSensors.h"
 #include <ScioSense_ENS16x.h> // ENS160 library https://github.com/sciosense/ens16x-arduino/tree/main
 
 
 
 
-VaseSensors::VaseSensors(uint8_t LIGHT_PIN, uint8_t SOIL_MOISTURE_PIN, unsigned int ENS160_I2C_ADDRESS, bool verbose ) {
+PotSensors::PotSensors(uint8_t LIGHT_PIN, uint8_t SOIL_MOISTURE_PIN, unsigned int ENS160_I2C_ADDRESS, bool verbose ) {
     this->LIGHT_PIN = LIGHT_PIN;
     this->SOIL_MOISTURE_PIN = SOIL_MOISTURE_PIN;
     this->ENS160_I2C_ADDRESS = ENS160_I2C_ADDRESS;
@@ -24,7 +24,7 @@ VaseSensors::VaseSensors(uint8_t LIGHT_PIN, uint8_t SOIL_MOISTURE_PIN, unsigned 
     ens160=new ENS160();
 }
 
-void VaseSensors::init() {
+void PotSensors::init() {
     if (verbose) {
         Serial.begin(9600);
         this->ens160->enableDebugging(Serial);            // Enables debugging for the esn160 on serial
@@ -63,17 +63,17 @@ void VaseSensors::init() {
     }
 }
 
-int VaseSensors::getSoilMoisture() const{
+int PotSensors::getSoilMoisture() const{
     return analogRead(this->SOIL_MOISTURE_PIN);
 }
 
 
-int VaseSensors::getLightLevel() const{
+int PotSensors::getLightLevel() const{
     return analogRead(this->LIGHT_PIN);
 }
 
 
-int VaseSensors::getTemperature(){
+int PotSensors::getTemperature(){
     if (millis() - this->last_aht > AHT_INTERVAL) {   // Update the humidity / temperature only if AHT_INTERVAL time has elapsed
         aht.getEvent(&this->humidity, &this->temp);
         this->last_aht = millis();
@@ -82,7 +82,7 @@ int VaseSensors::getTemperature(){
 }
 
 
-int VaseSensors::getHumidity(){ 
+int PotSensors::getHumidity(){ 
     if (millis() - this->last_aht > AHT_INTERVAL) {  // Update the humidity / temperature only if AHT_INTERVAL time has elapsed
         aht.getEvent(&this->humidity, &this->temp);
         this->last_aht = millis();
@@ -93,7 +93,7 @@ int VaseSensors::getHumidity(){
 
 
 //Air Quality Index (technically UBA but not actualy UBA as per specifications (UBA is for outdoor and uses different factors))
-uint8_t VaseSensors::getAQI(){
+uint8_t PotSensors::getAQI(){
     if (ens160->update() == RESULT_OK) {   // Checks if the sensor was able to read
         if (this->ens160->hasNewData()) {    // If the sensor has new data --> update the AQI
             this->AQI = (uint8_t)ens160->getAirQualityIndex_UBA();
@@ -109,14 +109,14 @@ uint8_t VaseSensors::getAQI(){
 
 
 // Sets OPMODE to DEEPSLEEP (<1mA), to use the sensor again you first need to reset it
-void VaseSensors::setENS160DeepSleep() {
+void PotSensors::setENS160DeepSleep() {
     this->ens160->setOperatingMode(ENS16X_OPERATING_MODE_DEEP_SLEEP);
 }
 
 
 
 // Resets the EnS160, NEEDS A WARMUP PHASE
-void VaseSensors::setENS160Reset() {
+void PotSensors::setENS160Reset() {
     while (this->ens160->init() != true)
     {
         delay(100);
