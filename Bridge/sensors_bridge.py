386f5f805e5957dccc488736692d5ff965e66ee0
Simone Serafini <63721643+SimoSera@users.noreply.github.com>
2025-05-09 00:05:49 +0200

    Update sensors_bridge.py

--- Bridge/sensors_bridge.py
+++ Bridge/sensors_bridge.py
@@ -1,6 +1,21 @@
 from dataclasses import dataclass, field
 from collections import deque
 import time
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+import os
+
+model_path="regressor_model.pkl"
+if os.path.exists(model_path):
+    try:
+        sensors_regress_model = joblib.load(model_path)
+        print("Model loaded successfully.")
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        sensors_regress_model = None
+else:
+    print("Model file not found.")
+    sensors_regress_model = None
 
 MAX_SENSORS_VALUES=10
 
@@ -12,14 +27,28 @@ class SensorData:
     moisture: list= field(default_factory=list)
     aqi: list= field(default_factory=list)
     timestamp : list= field(default_factory=list)
-    count: int = 0 
+    count: int = 0
 data_dict = {}
 
 
+HTTP_SERVER_URL="http://DESKTOP-RQ61EI4.local:8080/api/v1/PrbP2jlNNpjp8hJ5FcOV/telemetry" # TO FIX
 
-HTTP_SERVER_URL="thingsboard" # TO FIX
+# model = RandomForestRegressor()
+# model.fit(X, y)
 
+# # Save model to file
+# joblib.dump(model, 'regressor_model.pkl')
 
+def sensors_regress(data:SensorData):
+    reg_data=[data.temperature,data.humidity,data.light,data.moisture,data.aqi]
+    prediction=sensors_regress_model.predict(reg_data)
+    del data.temperature[0]
+    del data.humidity[0]
+    del data.light[0]
+    del data.moisture[0]
+    del data.aqi[0]
+    data.count-=1
+    return prediction
 
 import requests
 import json
@@ -29,9 +58,7 @@ from collections import defaultdict
 
 
 # TOPICS DEFINITION
-topic = "pot/+" 
- 
-
+topic = "pot/+"
 
 
 client_id= 'bridge-subscribe'
@@ -39,16 +66,14 @@ client_id= 'bridge-subscribe'
 
 # BROKER SETTINGS
 
-broker = '0.0.0.0'
+broker = '127.0.0.1'
 port = 1883
-
 
 
 # RECONNECT PROPERTIES
 FIRST_RECONNECT_DELAY = 1
 RECONNECT_RATE = 1.5
 MAX_RECONNECT_DELAY = 10
-
 def reconnection(client, userdata, rc):
     logging.info(f'{client.client_id} : Disconnected with result code: {rc}')
     reconnect_delay =FIRST_RECONNECT_DELAY
@@ -62,10 +87,10 @@ def reconnection(client, userdata, rc):
             return
         except Exception as err:
             logging.error(f'{client.client_id} : {err}. Reconnect failed. Retrying...')
-        
+
         reconnect_delay *= RECONNECT_RATE
         reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
-        
+
 
 
 def on_message(client,userdata,msg):
@@ -75,7 +100,7 @@ def on_message(client,userdata,msg):
     if len(parts) != 5 or len(topic_parts)!=2:
         print("Invalid message or topic:  message: ", message, "  topic:",msg.topic)
         return
-    
+
     id=topic_parts[1]
     d = data_dict.get(id)
     if d is None:
@@ -89,19 +114,23 @@ def on_message(client,userdata,msg):
     timestamp=time.time()
     d.timestamp.append(time.time())
     d.count+=1
+    payload = {
+        "ts": timestamp,
+        "values":{
+        "id": id,
+        "temperature": parts[0],
+        "humidity": parts[1],
+        "light": parts[2],
+        "moisture": parts[3],
+        "aqi": parts[4]}
+    }
+
+
+    if(d.count>=MAX_SENSORS_VALUES and sensors_regress_model!=None):
+        pred=sensors_regress(id)
+        payload["values"]["pred"]=pred
 
     try:
-        payload = {
-            "ts": timestamp,
-            "values":{
-            "id": id,
-            "temperature": parts[0],
-            "humidity": parts[1],
-            "light": parts[2],
-            "moisture": parts[3],
-            "aqi": parts[4]}
-        }
-
         response = requests.post(HTTP_SERVER_URL, json=payload, timeout=5)
         if response.status_code != 200:
             print(f"HTTP POST failed: {response.status_code} {response.text}")
@@ -109,9 +138,6 @@ def on_message(client,userdata,msg):
         print("HTTP request error:", e)
 
 
-    if(d.count>=MAX_SENSORS_VALUES):
-        predict_sensors(id)
-    
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc, properties):
@@ -136,8 +162,8 @@ def subscribe(client: mqtt_client):
 
 
 
-
-
 if __name__ == '__main__':
     client=connect_mqtt()
     subscribe(client)
+    client.loop_forever()
+
